3c5b87ba6b26843d68b59fa3dc890eb555be7de6
Meisenheimer <zeyu.wang.0117@outlook.com>
2024-06-15 18:15:11 +0800

    change easyx to ege

--- Gomoku/main.cpp
+++ Gomoku/main.cpp
@@ -8,10 +8,11 @@ void display(const Chess &chess);
 int main()
 {
     Integer win_cnt[3] = {0};
-    initgraph((WIDTH + 2) * GRID_SIZE, (HEIGHT + 2) * GRID_SIZE, SHOWCONSOLE);
-    setbkcolor(RGB(240, 155, 90));
-    settextcolor(RGB(0, 0, 0));
-    setlinecolor(RGB(128, 128, 128));
+    initgraph((WIDTH + 2) * GRID_SIZE, (HEIGHT + 2) * GRID_SIZE, INIT_RENDERMANUAL);
+    show_console();
+    setbkcolor_f(EGERGB(240, 155, 90));
+    settextcolor(EGERGB(0, 0, 0));
+    setlinecolor(EGERGB(128, 128, 128));
     while (true)
     {
         system("cls");
@@ -32,7 +33,7 @@ int main()
             }
             auto t1 = std::chrono::steady_clock::now();
             printf("-%s %ld %ld ", COLOR_NAME[chess.color], pos.first, pos.second);
-            printf("time = %d\n", std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count());
+            printf("time = %lld\n", std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count());
             chess.setPawn(pos.first, pos.second);
             res = chess.judge();
             if (res == BLACK_WIN || res == WHITE_WIN)
@@ -51,8 +52,7 @@ int main()
 
 inline void display(const Chess &chess)
 {
-    RECT rect;
-    BeginBatchDraw();
+    settextjustify(CENTER_TEXT, CENTER_TEXT);
     cleardevice();
     for (Integer h = 0; h <= HEIGHT; h++)
     {
@@ -64,17 +64,13 @@ inline void display(const Chess &chess)
     }
     for (Integer h = 0; h < HEIGHT; h++)
     {
-        rect = RECT{0, (h + 1) * GRID_SIZE, GRID_SIZE, (h + 2) * GRID_SIZE};
-        drawtext((LPCTSTR)std::to_string(h).c_str(), &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-        rect = RECT{(WIDTH + 1) * GRID_SIZE, (h + 1) * GRID_SIZE, (WIDTH + 2) * GRID_SIZE, (h + 2) * GRID_SIZE};
-        drawtext((LPCTSTR)std::to_string(h).c_str(), &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+        outtextrect(0, (h + 1) * GRID_SIZE, GRID_SIZE, GRID_SIZE, (LPCTSTR)std::to_string(h).c_str());
+        outtextrect((WIDTH + 1) * GRID_SIZE, (h + 1) * GRID_SIZE, GRID_SIZE, GRID_SIZE, (LPCTSTR)std::to_string(h).c_str());
     }
     for (Integer w = 0; w < WIDTH; w++)
     {
-        rect = RECT{(w + 1) * GRID_SIZE, 0, (w + 2) * GRID_SIZE, GRID_SIZE};
-        drawtext((LPCTSTR)std::to_string(w).c_str(), &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-        rect = RECT{(w + 1) * GRID_SIZE, (WIDTH + 1) * GRID_SIZE, (w + 2) * GRID_SIZE, (WIDTH + 2) * GRID_SIZE};
-        drawtext((LPCTSTR)std::to_string(w).c_str(), &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+        outtextrect((w + 1) * GRID_SIZE, 0, GRID_SIZE, GRID_SIZE, (LPCTSTR)std::to_string(w).c_str());
+        outtextrect((w + 1) * GRID_SIZE, (WIDTH + 1) * GRID_SIZE, GRID_SIZE, GRID_SIZE, (LPCTSTR)std::to_string(w).c_str());
     }
     for (Integer h = 0; h < HEIGHT; h++)
     {
@@ -82,16 +78,16 @@ inline void display(const Chess &chess)
         {
             if (chess(h, w) == BLACK_PAWN)
             {
-                setfillcolor(RGB(51, 51, 51));
+                setfillcolor(EGERGB(51, 51, 51));
                 solidcircle((2 * w + 3) * GRID_SIZE / 2, (2 * h + 3) * GRID_SIZE / 2, (Integer)(0.4 * GRID_SIZE));
             }
             if (chess(h, w) == WHITE_PAWN)
             {
-                setfillcolor(RGB(204, 204, 204));
+                setfillcolor(EGERGB(204, 204, 204));
                 solidcircle((2 * w + 3) * GRID_SIZE / 2, (2 * h + 3) * GRID_SIZE / 2, (Integer)(0.4 * GRID_SIZE));
             }
         }
     }
-    EndBatchDraw();
+    delay_ms(0);
     return;
 }
